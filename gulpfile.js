fe872fc623b2d7297179fd945a6fdd2f3cc60d44
James Bubb <jpbubb82@gmail.com>
2017-12-15 15:04:42 +0000

    Add gulpfile and src/dist folders

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+/**
+ * Gulpfile Setup for Building project
+ */
+
+const gulp = require('gulp');
+const gutil = require('gulp-util');
+
+ gulp.task('default', () => {
+    return gutil.log('Gulp is running!');
+ });
